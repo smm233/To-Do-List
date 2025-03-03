64bcfbb2a83c408c602b520cd3b7827a2b1036e6
shil-mil <shyshy122303@gmail.com>
2025-03-03 14:18:47 +0800

    Add script to uncheck, check, and delete task

--- script.js
+++ script.js
@@ -7,6 +7,17 @@ function addTask() {
         let li = document.createElement("li");
         li.innerHTML = inputBox.value;
         taskContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
     }
     inputBox.value = "";
 }
+
+taskContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+    } else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+    }
+}, false);
